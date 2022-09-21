667e7a23a50b584f086bd7a70f1a412620a3a654
Ден <sonnovdenis@gmail.com>
2022-09-22 01:55:40 +0400

    1-6 last tasks

--- src/test/java/PageSteps/MainPageSteps.java
+++ src/test/java/PageSteps/MainPageSteps.java
@@ -3,23 +3,15 @@ package PageSteps;
 import com.codeborne.selenide.Condition;
 import io.qameta.allure.Step;
 
-import static PageObject.PageElements.AutorizePage.*;
 import static PageObject.PageElements.MainPage.projectTestLink;
 import static PageObject.PageElements.MainPage.projectsMenuButton;
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.sleep;
-import static utils.Configuration.getConfigurationValue;
 
 public class MainPageSteps {
     @Step("Заходим в проект")
     public static void openProject(){
         projectsMenuButton.shouldBe(Condition.visible).click();
         projectTestLink.shouldBe(Condition.visible).click();
-        //sleep(5000);
     }
-//  @Test
-    //public static SelenideElement projectsMenuButton = $x("//a[@id='browse_link']");
-    //    public static SelenideElement projectTestLink = $x("//a[contains(text(), 'Test (TEST)')]");
 
 
 }
--- src/test/java/PageSteps/ProjectPageSteps.java
+++ src/test/java/PageSteps/ProjectPageSteps.java
@@ -20,18 +20,31 @@ public class ProjectPageSteps {
 
     public static void testSeleniumBug() {
         allTasksAndFilters.shouldBe(Condition.visible).click();
+        //sleep(2000);
         inputNameTask.shouldBe(Condition.visible).sendKeys("TestSelenium_bug");
+        //sleep(2000);
         searchButton.click();
+       // sleep(2000);
         assertStatus.shouldHave(Condition.text("Сделать"));
+        //sleep(2000);
         assertVersion.shouldHave(Condition.text("Version 2.0"));
+        //sleep(2000);
         createLink.click();
+        //sleep(2000);
         typeTask.click();
+        //sleep(2000);
         typeTask.sendKeys(Keys.DELETE);
+        //sleep(2000);
         typeTask.shouldBe(Condition.visible).setValue("О");
+        //sleep(2000);
         typeTask.pressEnter();
+       // sleep(2000);
         themeTask.click();
+        //sleep(2000);
         themeTask.sendKeys("Не меняется цена после смены валюты");
+        //sleep(2000);
         descriptionTextButton.click();
+        //sleep(2000);
         textAreaDescription.sendKeys("Шаги \n" +
                 "\n" +
                 "1) открытие сайта\n" +
@@ -52,26 +65,45 @@ public class ProjectPageSteps {
                 "\n" +
                 "Цена не изменилась");
         versionAreaDescription.click();
+        //sleep(2000);
         propertyTask.click();
+        //sleep(2000);
         propertyTask.sendKeys(Keys.DELETE);
+        //sleep(2000);
         propertyTask.shouldBe(Condition.visible).sendKeys("H");
+        //sleep(2000);
         propertyTask.pressEnter();
+        //sleep(2000);
         labelTask.click();
+        //sleep(2000);
         labelTask.sendKeys("sonnov_test");
+        //sleep(2000);
         labelTask.pressEnter();
+        //sleep(2000);
         textEnvironment.sendKeys("Windows 10  Google chrome Версия 101.0.4951.67 (Официальная сборка), (64 бит) Сайт открыт по ссылке : * http://shop.findbug.ru/*");
+        //sleep(2000);
         sunkVersion.click();
+        sleep(2000);
         task.sendKeys("TEST-21967");
+        //sleep(2000);
         task.pressEnter();
+        sleep(2000);
         sprint.sendKeys("1");
+        //sleep(2000);
         sprint.pressEnter();
+        //sleep(2000);
         create.click();
+        //sleep(2000);
         myTask.click();
+        //sleep(2000);
         statusInWork.click();
+        //sleep(2000);
         businessProcess.click();
+        //sleep(2000);
         statusWin.click();
+        //sleep(2000);
         getAssertStatusMyTask.shouldHave(Condition.text("Готово"));
-        sleep(5000);
+        //sleep(5000);
 
     }
 }
